902df4a90c0b870884803cdc029e142bcce52e05
James A. Duncan <james@kinburnstreet.consulting>
2025-03-19 16:14:37 +0000

    DOM Template Literals

---
+++ index.mjs
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT | NodeFilter.SHOW_ELEMENT);
+
+    while( treeWalker.nextNode() ) {
+        const node = treeWalker.currentNode;
+        if ( node.nodeType == 3 ) // plain old text nodes
+            node.textContent = eval("`"+node.textContent+"`");
+        else { // attributes
+            for ( const attr of node.attributes ) {
+                console.log( attr.value );
+                try {
+                    attr.value = eval("`" + attr.value + "`");
+                } catch(e) {
+                    // if we can't do template literals, oh well.
+                }
+            }
+        }
+    }
+});
+
