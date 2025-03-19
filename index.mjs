9f6b4f619910eaed222f9d685402692c503ed395
James A. Duncan <james@kinburnstreet.consulting>
2025-03-19 19:24:29 +0000

    factored out the templating engine so it can be used elsewhere as well.

--- index.mjs
+++ index.mjs
@@ -1,19 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT | NodeFilter.SHOW_ELEMENT);
+class TemplateEngine {
 
-    while( treeWalker.nextNode() ) {
-        const node = treeWalker.currentNode;
-        if ( node.nodeType == 3 ) // plain old text nodes
-            node.textContent = eval("`"+node.textContent+"`");
-        else { // attributes
-            for ( const attr of node.attributes ) {
-                try {
-                    attr.value = eval("`" + attr.value + "`");
-                } catch(e) {
-                    // if we can't do template literals, oh well.
+    evaluate( expr, context ) {
+        const code = "'use strict'; `"+expr+"`";
+        try {
+            return eval?.apply( context, [ code ] );
+        } catch (e) {
+            return '';
+        }
+    }
+
+    render( node, context ) {
+        const treeWalker = document.createTreeWalker( node, NodeFilter.SHOW_TEXT | NodeFilter.SHOW_ELEMENT );
+        "use strict";
+        while( treeWalker.nextNode() ) {
+            const node = treeWalker.currentNode;
+            if ( node.nodeType === 3) {
+                node.textContent = this.evaluate( node.textContent );    
+            } else {
+                for ( const attr of node.attributes ) {
+                    attr.value = this.evaluate( attr.value, context );
                 }
             }
         }
     }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const engine = new TemplateEngine();
+    engine.render( document.body, {} );
 });
 
